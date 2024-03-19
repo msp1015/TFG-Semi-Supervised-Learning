8a17d88223a9e787fcdf71e03de0d0a4bab88519
msp1015 <msp1015@alu.ubu.es>
2024-03-19 01:54:23 +0100

    Necesario para poder ejecutar en local

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='algoritmos',
+    version='1.0',
+    packages=find_packages(),
+    author='David Martínez Acha',
+    description='Paquete de algoritmos Semi-Supervisados'
+)
