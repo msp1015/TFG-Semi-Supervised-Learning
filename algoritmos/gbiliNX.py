9513d2f604a65b924627fb23ae0f063b2061991c
msp1015 <msp1015@alu.ubu.es>
2024-04-30 01:33:07 +0200

    Correciones

--- algoritmos/gbiliNX.py
+++ algoritmos/gbiliNX.py
@@ -4,14 +4,13 @@ import numpy as np
 from scipy.spatial import distance_matrix
 from collections import defaultdict
 
-class GraphKNNSolverNetworkX:
+class GbiliNX:
     def __init__(self, vertices, labels, K):
         self.vertices = vertices 
         self.labels = labels     
         self.K = K              
         self.G = nx.Graph()       
         self.distance_matrix = distance_matrix(vertices, vertices)
-        print(self.distance_matrix)
         
     def visualize_graph(self, title):
         pos = {i: self.vertices[i] for i in range(len(self.vertices))}
@@ -33,7 +32,22 @@ class GraphKNNSolverNetworkX:
             for _, idx in distances[:self.K]:
                 self.G.add_edge(i, idx)
         # self.visualize_graph("Graph After k-NN")
+        pos = {i: self.vertices[i] for i in range(len(self.vertices))}
 
+        plt.figure(figsize=(8, 6))
+        nx.draw(self.G, pos, with_labels=True, node_color='skyblue', edge_color='gray', node_size=500, font_size=15, font_weight='bold')
+
+        # Resaltar los nodos etiquetados
+        labelled_nodes = [node for node in self.G.nodes() if node in labels]
+        nx.draw_networkx_nodes(self.G, pos, nodelist=labelled_nodes, node_color='salmon')
+
+        # Dibujar las etiquetas de los nodos etiquetados
+        label_pos = {node: (pos[node][0], pos[node][1] + 0.05) for node in labelled_nodes}  # Ajusta la posición de la etiqueta
+        nx.draw_networkx_labels(self.G, label_pos, labels={node: labels[node] for node in labelled_nodes}, font_color='darkred')
+
+        plt.title('Visualización del Grafo con NetworkX')
+        plt.show()
+        
     def find_mknn(self):
         mknn_list = defaultdict(list)
         for i in self.G.nodes:
@@ -65,7 +79,7 @@ class GraphKNNSolverNetworkX:
             if not comp_label[frozenset(comp)]:
                 for v in comp:
                     for k in self.G.nodes():
-                        if k not in comp and comp_label[frozenset(nx.node_connected_component(self.G, k))]:
+                        if k not in comp and comp_label.get(frozenset(nx.node_connected_component(self.G, k)), False):
                             self.G.add_edge(v, k)
                             break
     
@@ -80,9 +94,9 @@ class GraphKNNSolverNetworkX:
 
 
 #np.random.seed(0)
-vertices = np.random.rand(10, 2)  
-labels = {0: 'A', 5: 'B'} 
-solver = GraphKNNSolverNetworkX(vertices, labels, 3)
+vertices = np.random.rand(35, 2)  
+labels = {0: 'A', 5: 'B', 7: 'C', 9: 'D', 3: 'E', 8: 'F', 12: 'G', 15: 'H', 18: 'I', 20: 'J', 22: 'K', 25: 'L', 27: 'M', 29: 'N'} 
+solver = GbiliNX(vertices, labels, 2)
 graph = solver.solve()
 # Posiciones de los nodos basadas en sus coordenadas (si son representativas de la estructura del grafo)
 pos = {i: vertices[i] for i in range(len(vertices))}
