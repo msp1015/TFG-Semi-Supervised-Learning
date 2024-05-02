9f1b9445f5aaa63277abd737e67920eb7ee20d49
msp1015 <msp1015@alu.ubu.es>
2024-05-02 03:10:32 +0200

    Gbili actualizado con construccion e inferencia

--- algoritmos/gbili.py
+++ algoritmos/gbili.py
@@ -1,105 +1,273 @@
-"""Este módulo contiene la implementación del algoritmo CoForest
-
-@Autor:     Mario Sanz Pérez
-@Fecha:     23/04/2024
-@Versión:   1.2
-@Nombre:    Gbili.py
-"""""
-
+from matplotlib import pyplot as plt
+import networkx as nx
+from collections import deque, defaultdict
 import numpy as np
 from scipy.spatial import distance_matrix
-from collections import defaultdict, deque
 
 class Gbili:
-    """_summary_
-    """
-    def __init__(self, vertices, labels, K):
-        self.vertices = vertices
-        self.labels = labels
+    def __init__( self, datos_se, datos_e, etiquetas_e, K):
+        self.datos_sin_etiquetar = datos_se
+        self.datos_etiquetados = datos_e
         self.K = K
-        self.distance_matrix = distance_matrix(self.vertices, self.vertices)
-        self.graph = defaultdict(list)
-    
-    def find_knn(self):
-        n = len(self.vertices)
-        knn_list = defaultdict(list)
-        for i in range(n):
-            distances = [(self.distance_matrix[i][j], j) for j in range(n) if i != j]
-            distances.sort()
-            knn_list[i] = [idx for _, idx in distances[:self.K]]
-        print(knn_list)
-        return knn_list
-    
-    def find_mknn(self, knn_list):
-        mknn_list = defaultdict(list)
-        for i in knn_list:
-            for j in knn_list[i]:
-                if i in knn_list[j]:
-                    mknn_list[i].append(j)
-        print(mknn_list)
-        return mknn_list
-    
-    def connect_min_distance(self, mknn_list):
-        for i in mknn_list:
-            for j in mknn_list[i]:
-                min_distance = float('inf')
-                min_link = None
-                for l in self.labels:
-                    d = self.distance_matrix[i][j] + self.distance_matrix[j][l]
-                    if d < min_distance:
-                        min_distance = d
-                        min_link = (i, j)
-                if min_link:
-                    self.graph[min_link[0]].append(min_link[1])
-                    self.graph[min_link[1]].append(min_link[0])
-        print(self.graph)
+        #Union de los nodos etiquetados y no etiquetados
+        self.vertices = np.concatenate((self.datos_etiquetados, self.datos_sin_etiquetar), axis=0)
         
-    def find_components(self):
-        visited = {}
-        component = {}
-        comp_id = 0
-        def bfs(v):
-            queue = deque([v])
-            visited[v] = True
-            component[v] = comp_id
-            while queue:
-                current = queue.popleft()
-                for neighbor in self.graph[current]:
-                    if not visited.get(neighbor, False):
-                        visited[neighbor] = True
-                        component[neighbor] = comp_id
-                        queue.append(neighbor)
+        self.nodos_etiquetados = range(len(self.datos_etiquetados))
+        self.matriz_distancias = distance_matrix(self.vertices, self.vertices)
+        print("MATRIZ DE DISTANCIAS: ", self.matriz_distancias)
+        self.grafo = {}
         
-        for v in range(len(self.vertices)):
-            if v not in visited:
-                bfs(v)
-                comp_id += 1
-        print(component)
-        return component
-    
-    def link_components(self, component):
-        comp_label = {}
-        for comp in set(component.values()):
-            comp_label[comp] = any(self.labels.get(idx) for idx in component if component[idx] == comp)
+        #Inferencia
+        self.etiquetas_etiquetados = etiquetas_e
+        self.n_categorias = len(np.unique(self.etiquetas_etiquetados))
+        print(np.unique(self.etiquetas_etiquetados))
+        self.Y = self.inicializar_Y()
         
-        for v in range(len(self.vertices)):
-            if not comp_label[component[v]]:
-                for k in self.graph[v]:
-                    if comp_label[component[k]]:
-                        self.graph[v].append(k)
-                        self.graph[k].append(v)
-                        break
-    
-    def solve(self):
-        knn_list = self.find_knn()
-        mknn_list = self.find_mknn(knn_list)
-        self.connect_min_distance(mknn_list)
-        component = self.find_components()
-        self.link_components(component)
-        return self.graph
+    def construir_grafo(self):
+        """_summary_
+        """
+        
+        lista_knn = self.encuentra_knn()
+        print("LISTA KNN: ", lista_knn)
+        print()
+        lista_mknn = self.encuentra_mknn(lista_knn)
+        print("LISTA MKNN: ", lista_mknn)
+        print()
+        self.conectar_minima_distancia(lista_mknn)
+        #A esta altura debe haber varios subgrafos desconectados en el grafo
+        print("GRAFO: ", self.grafo)
+        print()
+        
+        grafo_1 = self.grafo.copy()
+        # Componente = subgrafo
+        componentes = self.encontrar_componentes()
+        print("COMPONENTES: ", componentes)
+        print()        
+        self.conectar_componentes(componentes, lista_knn)
+        print("GRAFO CON COMPONENTES CONECTADOS: ", self.grafo)
+        grafo_2 = self.grafo
+        
+        if grafo_1 == grafo_2:
+            print("Los grafos son iguales antes y despues  de conectar los componentes")
+        
+        # Configurar la figura y los ejes
+        fig, axs = plt.subplots(1, 2, figsize=(12, 6))  # 1 fila, 2 columnas
+        
+        self.dibujar_grafo(grafo_1, axs[0], "Grafo antes de conectar componentes")
+        self.dibujar_grafo(grafo_2, axs[1], "Grafo después de conectar componentes")
+        plt.show()
+        return self.grafo
+        
+    def encuentra_knn(self):
+        """ Encuentra los k vecinos más cercanos de cada vértice.
+        <<knn>>: k-nearest neighbors
 
-vertices = np.random.rand(10, 2)
-labels = {0: 'A', 5: 'B'}
-solver = Gbili(vertices, labels, 3)
-graph = solver.solve()
-print(graph)
+        Returns:
+            dict: contiene los k vecinos más cercanos de cada vértice.
+        """
+        n = len(self.vertices)
+        lista_knn = {}
+        for i in range(n):
+            # Guarda una tupla con la distancia y el indice del vertice
+            distancias = [(self.matriz_distancias[i][j], j) for j in range(n) if i != j]
+            distancias.sort()
+            # Seleccionar los K vecinos más cercanos
+            lista_knn[i] = [ind_v for _, ind_v in distancias[:self.K]]
+        return lista_knn
+    
+    def encuentra_mknn(self, lista_knn):
+        """ Para los vértices en la lista de k-NN
+        encuentra los pares mutuos de vecinos más cercanos.
+        Mutuo k-NN: Si i es vecino más cercano de j y 
+                    j es vecino más cercano de i.
+        Args:
+            lista_knn (dict): contiene los k vecinos más cercanos de cada vértice.
+
+        Returns:
+            dict: contiene los pares mutuos de vecinos más cercanos.
+        """
+        lista_mknn = {}
+        for i in lista_knn:
+            lista_mknn[i] = []
+            for j in lista_knn[i]:
+                if i in lista_knn[j]:
+                    lista_mknn[i].append(j)
+        return lista_mknn
+    
+    def conectar_minima_distancia(self, lista_mknn):
+        """ Conecta los vértices con la mínima distancia.
+        Almacena esos enlaces en otro diccionario que representa el grafo.
+        Args:
+            lista_mknn (dict): contiene los pares mutuos de vecinos más cercanos.
+        """
+        for i in lista_mknn:
+            for j in lista_mknn[i]:
+                min_distancia = float('inf')
+                min_enlace = None
+                for l in self.nodos_etiquetados:
+                    d = self.matriz_distancias[i][j] + self.matriz_distancias[j][l]
+                    if d < min_distancia:
+                        min_distancia = d
+                        min_enlace = (i, j)
+                if min_enlace:
+                    # Si el nodo no está en el grafo, se agrega
+                    if min_enlace[0] not in self.grafo:
+                        self.grafo[min_enlace[0]] = []
+                    # Se conectan los nodos
+                    self.grafo[min_enlace[0]].append(min_enlace[1])
+        
+    def encontrar_componentes(self):
+        """ Encuentra los componentes del grafo.
+        Utiliza BFS (Breadth First Search) o busqueda en anchura 
+        para encontrar los componentes.
+        
+        Returns:
+            dict: contiene los componentes del grafo.
+        """
+        visitados = {}
+        componentes = {}
+        id_comp = 0
+        def bfs(v):
+            """Realiza una búsqueda en anchura a partir de un vértice dado.
+
+            Esta función implementa el algoritmo de búsqueda en anchura
+            (BFS por sus siglas en inglés) en un grafo. 
+            Comienza la búsqueda desde el vértice 'v' y visita todos los 
+            vértices alcanzables desde 'v' en orden de 
+            distancia. Utiliza una cola para mantener un seguimiento de los 
+            vértices a visitar.
+
+            Args:
+                v (int): El vértice de inicio para la búsqueda en anchura.
+            """
+            cola = deque([v])
+            visitados[v] = True
+            componentes[v] = id_comp
+            while cola:
+                actual = cola.popleft()
+                for vecino in self.grafo.get(actual, []):
+                    if not visitados.get(vecino, False):
+                        visitados[vecino] = True
+                        componentes[vecino] = id_comp
+                        cola.append(vecino)
+        
+        for v in range(len(self.vertices)):
+            if v not in visitados:
+                bfs(v)
+                id_comp += 1
+        return componentes
+                    
+    def conectar_componentes(self, comp, lista_knn):
+        """
+        Conecta los componentes del grafo basándose en la presencia de etiquetas.
+        
+        Args:
+            comp (dict): contiene los componentes del grafo con cada nodo mapeado a su componente.
+            lista_knn (dict): contiene los k vecinos más cercanos de cada vértice.
+        """
+        # Invertir el diccionario de componentes para agrupar nodos por componente
+        comp_a_nodos = defaultdict(list)
+        for nodo, componente in comp.items():
+            comp_a_nodos[componente].append(nodo)
+        
+        # Determinar si cada componente tiene nodos etiquetados
+        comp_etiquetados = {}
+        for componente, nodos in comp_a_nodos.items():
+            comp_etiquetados[componente] = any(nodo in self.nodos_etiquetados for nodo in nodos)
+        
+        # Conectar componentes según las condiciones dadas
+        for v in range(len(self.vertices)):
+            componente_v = comp[v]
+            # Verificar si la componente de v no tiene nodos etiquetados
+            if not comp_etiquetados[componente_v]:
+                for vk in lista_knn[v]:
+                    componente_vk = comp[vk]
+                    # Verificar si la componente de vk tiene nodos etiquetados
+                    if comp_etiquetados[componente_vk]:
+                        # Asegurarse de que ambos nodos estén en el grafo y conectarlos
+                        if v not in self.grafo:
+                            self.grafo[v] = []
+                        if vk not in self.grafo[v]:
+                            self.grafo[v].append(vk)
+                            print("Conectando nodos: ", v, vk)
+
+           
+    def dibujar_grafo(self, grafo, ax, titulo):
+        # Crear un objeto grafo de NetworkX
+        G = nx.Graph()
+
+        # Añadir los nodos y las aristas desde el diccionario
+        for nodo, vecinos in grafo.items():
+            G.add_node(nodo)  # Aunque no es necesario añadir explícitamente los nodos
+            for vecino in vecinos:
+                G.add_edge(nodo, vecino)
+                
+        import matplotlib.pyplot as plt
+
+        # Dibujar el grafo
+        nx.draw(G, ax=ax, with_labels=True, node_color='lightblue', edge_color='gray', node_size=30, font_size=10, font_weight='bold')
+        ax.set_title(titulo)
+        
+    def inicializar_Y(self):
+        """ Inicializa la matriz de etiquetas Y (mascara). 
+        Se inicializa con ceros y se asigna un 1 en la columna correspondiente a la etiqueta."""
+        Y = np.zeros((len(self.vertices), self.n_categorias))
+        
+        for i, label in enumerate(self.etiquetas_etiquetados):
+            Y[i, label] = 1
+        return Y       
+    
+    def construir_matriz_afinidad(self, sigma=1):
+        # sigma es el parámetro de escala para la función exponencial
+        W = np.exp(-self.matriz_distancias**2 / (2 * sigma**2))
+        np.fill_diagonal(W, 0)  # Poner la diagonal a 0
+        return W  
+        
+    
+    def normalizar_afinidad(self, W):
+        D = np.diag(W.sum(axis=1))
+        D_inversa = np.diag(1 / np.sqrt(D.diagonal()))
+        S = D_inversa @ W @ D_inversa
+        return S   
+        
+    def iterar_F(self, S, alpha=0.5, tol=1e-6):
+        F = self.Y.copy()
+        while True: # TODO agregar condición de parada, ya que puede no converger o tardar mucho
+            F_next = alpha * S @ F + (1 - alpha) * self.Y
+            if np.linalg.norm(F_next - F) < tol:
+                break
+            F = F_next
+        return F
+        
+    def predecir_etiquetas(self, F):
+        return np.argmax(F, axis=1)
+
+    def inferir_etiquetas(self):
+        W = self.construir_matriz_afinidad()
+        S = self.normalizar_afinidad(W)
+        F_final = self.iterar_F(S)
+        return self.predecir_etiquetas(F_final)
+    
+    
+    
+    
+    
+## Ejemplo de uso
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+iris = load_iris()
+
+x = iris.data
+y = iris.target
+K = 5
+
+L, U, L_, U_ = train_test_split(x, y, test_size=0.7, stratify=y)
+print(L_)
+solver = Gbili(U, L, L_, K)
+grafo = solver.construir_grafo()
+predicciones = solver.inferir_etiquetas()
+print("Predicciones: ", predicciones)
+e = np.concatenate((L_, U_))
+print("Etiquetas reales: ", e)
+print("Accuracy: ", np.mean(predicciones == e))
+
